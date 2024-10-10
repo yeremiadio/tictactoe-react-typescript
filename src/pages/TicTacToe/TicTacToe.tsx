e37ceac9009b7a52affa718f9aef3f2d760ddfb4
keiko020509 <keikomelody0205009@gmail.com>
2024-10-10 16:23:17 +0700

    change

--- src/pages/TicTacToe/TicTacToe.tsx
+++ src/pages/TicTacToe/TicTacToe.tsx
@@ -33,39 +33,39 @@ const TicTacToe = () => {
   const checkWin = () => {
     if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
     {
-      won(data);
+      won(data[2]);
     }
     else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
     {
-      won(data);
+      won(data[5]);
     }
     else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
     {
-      won(data);
+      won(data[8]);
     }
     else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
     {
-      won(data);
+      won(data[6]);
     }
     else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
     {
-      won(data);
+      won(data[7]);
     }
     else if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
     {
-      won(data);
+      won(data[8]);
     }
     else if(data[0]===data[4] && data[4]===data[8] && data[8]!=="")
     {
-      won(data);
+      won(data[8]);
     }
     else if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
     {
-      won(data);
+      won(data[2]);
     }
     else if(data[2]===data[4] && data[4]===data[6] && data[6]!=="")
     {
-      won(data);
+      won(data[6]);
     }
   }
 
@@ -74,12 +74,12 @@ const TicTacToe = () => {
     if(winner==="x")
     {
       //@ts-ignore
-      titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon}>`;
+      titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon}> Wins`;
     }
     else
     {
       //@ts-ignore
-      titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon}>`;
+      titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon}> Wins`;
     }
   }
 

2a8368547504c64f55ac82414843ab813a800f90
DeathShadowCybear <darkshadowcybear@gmail.com>
2024-10-11 12:43:52 +0700

    Removed Console Log

--- src/pages/tictactoe/index.tsx
+++ src/pages/tictactoe/index.tsx
@@ -46,47 +46,47 @@ const Tictactoe = () => {
    if(data[0]==data[1] && data[1]==data[2] && data[2]!=="")
    {
     won(data[2]);
-    console.log('this is wrong line 49')
+    
    }
    else if(data[3]==data[4] && data[4]==data[5] && data[5]!=="")
    {
     won(data[5]);
-    console.log('this is wrong line 53')
+    
    }
    else if(data[6]==data[7] && data[7]==data[8] && data[8]!=="")
     {
      won(data[8]);
-     console.log('this is wrong line 57')
+     
     }
   else if(data[0]==data[3] && data[3]==data[6] && data[6]!=="")
       {
        won(data[6]);
-       console.log('this is wrong line 61')
+       
       }
 
   else if(data[3]==data[4] && data[4]==data[7] && data[7]!=="")
         {
          won(data[7]);
-         console.log(`thid is wrong line 67`)
+         
         } 
 
    else if(data[2]==data[5] && data[5]==data[8] && data[8]!=="")
           {
            won(data[8]);
-           console.log('this is wrong line 75')
+          
           }
    else if(data[0]==data[1] && data[1]==data[2] && data[2]!=="")
    {
     won(data[2]);
-    console.log('this is wrong line 86')
+    
    }
    else if(data[2]==data[4] && data[4]==data[6] && data[6]!=="")
     {
      won(data[6]);
-     console.log('this is wrong line 91')
+     
     } else if (data[0] !== data[1] && data[2] === data[3] && data[3] !== data[4] && data[5] === '' && data[6] !== data[7])
     {
-      console.log('this is wrong line 94')
+      
       return;
     }
   }
